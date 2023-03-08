ef68c083010876e68b3b46baf23edfc1265b469c
zkr911 <63081032+zkr911@users.noreply.github.com>
2023-03-08 02:35:29 -0300

    arreglo

--- pruebacryp.php
+++ pruebacryp.php
@@ -8,22 +8,7 @@ $texto = "texto de prueba";
 
 
 ?>
-<link rel="stylesheet" href="componentes/css/stylePublicidad.css">
-
-
-<figure id="photo" title="¿Qué hay, bro?" tooltip-dir="left">
-  <img src="https://s-media-cache-ak0.pinimg.com/564x/c0/f0/9f/c0f09f6c4d59f94f60f27ba6a05b8f57.jpg"/>
-</figure>
 
 
 
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-  let figure = document.getElementById('photo');
-  let directions = document.getElementById('directions');
 
-  directions.addEventListener('change', function () {
-    figure.setAttribute('tooltip-dir', this.value);
-  });
-});
-</script>
